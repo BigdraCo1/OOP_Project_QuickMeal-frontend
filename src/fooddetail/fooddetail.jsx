ca000dab4dfd785fd887f769b4cbab1c8d717a64
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2024-03-11 23:01:10 +0700

    Update fooddetail.css and fooddetail.jsx

--- src/fooddetail/fooddetail.jsx
+++ src/fooddetail/fooddetail.jsx
@@ -57,13 +57,14 @@ function FoodDetail(){
     return (
       <>
       { isLoading && <div>.....Loading.....</div> }
-      { !isLoading && <div className='midText'>
+      { !isLoading && 
+      <div className='midText'>
         <HomeButton/>
         <BasketButton/>
         <div className='detailbox'>
-        <h2>{fooddetail.food_name}</h2>
-        <h3>{fooddetail.food_type}</h3>
-        <h3>{fooddetail.food_price} ฿</h3>
+        <h2 className='text-[1.2rem] font-bold'>{fooddetail.food_name}</h2>
+        <h3 className='text-[1.2rem]'>{fooddetail.food_type}</h3>
+        <h3 className='text-[1.2rem]'>{fooddetail.food_price} ฿</h3>
         <div>
           {Object.entries(fooddetail.food_size).map(([size, cost]) => (
             <div key={size}>
@@ -79,8 +80,12 @@ function FoodDetail(){
             <input type="text" value={quantity} readOnly />
             <button onClick={() => handleQuantityChange(1)}>+</button>
         </div>
-        <button onClick={addToBasket}>Add to Basket</button>
-      </div>
+        <button className='text-slate-700 text-[1.1rem] font-bold bg-slate-300 
+        border-2 border-slate-300 px-[1rem] py-[0.2rem] rounded-md shadow-md
+        hover:bg-slate-400 hover:border-slate-400 hover:shadow-lg hover:text-slate-200
+        transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105' 
+        onClick={addToBasket}>Add to Basket</button>
+        </div>
       </div>} 
     </>  
     ) 
