c260987c15e1098b2af85a213ae54ffd5fefa17f
BigdraCo1 <pakawat.tab@gmail.com>
2024-03-12 11:35:22 +0700

    add search function in customer page

--- src/showres/restaurants.jsx
+++ src/showres/restaurants.jsx
@@ -1,56 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import './restaurants.css'
-import { Link, useParams } from 'react-router-dom'
-import RestaurantCard from '../components/RestaurantCard';
-import BasketButton from '../components/BasketButton';
-import HomeButton from '../components/HomeButton';
-import ProfileButton from '../components/ProfileButton'
-import api from '../Header/API'
+import React, { useState, useEffect } from "react";
+import "./restaurants.css";
+import { Link, useParams } from "react-router-dom";
+import RestaurantCard from "../components/RestaurantCard";
+import BasketButton from "../components/BasketButton";
+import HomeButton from "../components/HomeButton";
+import ProfileButton from "../components/ProfileButton";
+import api from "../Header/API";
+import lensLogo from "../assets/lens.svg";
 
-const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = "http://127.0.0.1:8000";
 
-function CustomerRestaurants(){
-  const {id} = useParams()
-  const [restaurants, setRestaurants] = useState({});
-  const [isLoading, setIsLoading]= useState(true)
+function CustomerRestaurants() {
+  const { id } = useParams();
+  const [key, setKey] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  async function ShowAllRes() {
+  async function fetchRestaurant(key) {
     try {
-      const response = await api.get(`${BASE_URL}/show/restaurant/mainpage`)
-      setRestaurants(response.data)
-      setIsLoading(false)
+      let response;
+      if (key === "") {
+        response = await api.get(`${BASE_URL}/show/restaurant/mainpage`);
+      } else {
+        response = await api.get(
+          `${BASE_URL}/search/menu_and_restaurant/${key}`
+        );
+      }
+      setRestaurants(response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.log("error",error)
+      console.log("error", error);
     }
   }
-  
-  useEffect(() => {ShowAllRes()}, []);
+
+  const handleChange = (event) => {
+    setKey(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetchRestaurant(key);
+  };
+  useEffect(() => {
+    fetchRestaurant(key);
+  }, []);
 
   return (
     <>
-    { isLoading && <div>.....Loading.....</div> }
-    { !isLoading && 
-    <div>
-      <HomeButton id={id}/>
-      <BasketButton id={id}/>
-      <ProfileButton id={id}/>
-      <div className="midText">
-        <h1>List of Restaurants</h1>
-      </div>
-      <div className='container'>
-        {restaurants.map( (restaurant, index) => (
-          <div key={index}>
-            <Link to = {`/${id}/show/${restaurant._Restaurant__restaurant_id}/menu`}>
-              <RestaurantCard name = {restaurant._Restaurant__name_restaurant}
-              rating = {restaurant._Restaurant__rate}
-              location = {restaurant._Restaurant__restaurant_location} />
-            </Link>
+      {isLoading && <div className="text-center">Loading...</div>}
+      {!isLoading && (
+        <div className="container mx-auto px-4 py-8">
+          <div className="flex justify-between items-center mb-4">
+            <HomeButton id={id} />
+            <BasketButton id={id} />
+            <ProfileButton id={id} />
           </div>
-        ))}
-      </div>
-    </div>}
+          <br />
+          <br />
+          <div className="flex gap-2 relative shadow-md sm:rounded-lg">
+            <div className="relative mt-0">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <img src={lensLogo} className="logo" alt="Star logo" />
+              </div>
+              <input
+                className="mt-1 px-3 py-2 border border-gray-300 shadow-sm bg-gray-50 focus:outline-none focus:border-green-200 text-gray-900 text-sm rounded-lg block w-80 pl-10 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black"
+                type="text"
+                placeholder="Search for restaurants..."
+                value={key}
+                onChange={handleChange}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSubmit();
+                  }
+                }}
+              />
+            </div>
+            <button
+              className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600"
+              onClick={handleSubmit}
+            >
+              Search
+            </button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            {restaurants.map((restaurant, index) => (
+              <div key={index}>
+                <Link
+                  to={`/${id}/show/${restaurant._Restaurant__restaurant_id}/menu`}
+                >
+                  <RestaurantCard
+                    name={restaurant._Restaurant__name_restaurant}
+                    rating={restaurant._Restaurant__rate}
+                    location={restaurant._Restaurant__restaurant_location}
+                  />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </>
-  )
-};
+  );
+}
 
 export default CustomerRestaurants;
